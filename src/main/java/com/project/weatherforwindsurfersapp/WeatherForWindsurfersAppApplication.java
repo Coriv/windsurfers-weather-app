7ad236f3194b3f165b9ab120ceb411bbbef75529
Sebastian Boron <sebastian.boron8@gmail.com>
2023-07-08 21:16:49 +0200

    initial commit

---
+++ src/main/java/com/project/weatherforwindsurfersapp/WeatherForWindsurfersAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.weatherforwindsurfersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherForWindsurfersAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherForWindsurfersAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/weatherforwindsurfersapp/WeatherForWindsurfersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.weatherforwindsurfersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForWindsurfersAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
