2049f6d68e5ef6fc96294e7fdb2de450b4eb588a
Sebastian Boron <sebastian.boron8@gmail.com>
2023-07-11 00:05:49 +0200

    refactored

--- src/main/java/com/project/weatherforwindsurfersapp/service/ExternalDataService.java
+++ src/main/java/com/project/weatherforwindsurfersapp/service/ExternalDataService.java
@@ -3,9 +3,7 @@ package com.project.weatherforwindsurfersapp.service;
 import com.project.weatherforwindsurfersapp.config.AdminConfig;
 import com.project.weatherforwindsurfersapp.dto.WeatherData;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -14,23 +12,13 @@ import java.nio.charset.StandardCharsets;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class ExternalDataService {
 
     private final RestTemplate restTemplate;
     private final AdminConfig config;
 
     public WeatherData fetchWeatherDataByCity(String cityName) {
-
-        try{
-            log.info("Rest call for WeatherData for parameter: " + cityName);
-            var weatherData = restTemplate.getForObject(prepareUri(cityName), WeatherData.class);
-            log.info("Retrieved new WeatherData object for parameter: " + cityName);
-            return weatherData;
-        } catch (RestClientException e) {
-            log.error("RestClint Exception for parameter: " + cityName, e.getMessage());
-        }
-        return null;
+        return restTemplate.getForObject(prepareUri(cityName), WeatherData.class);
     }
 
     private URI prepareUri(String cityName) {
